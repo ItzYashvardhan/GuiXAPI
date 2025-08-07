cb08cba991c6c29e63a8a53adad22d6297d2b91d
Yashvardhan76 <itsyashvardhan76@gmail.com>
2025-08-07 14:30:04 +0530

    small patch fix for FrameColor.kt

--- src/main/kotlin/net/justlime/limeframegui/utilities/FrameColor.kt
+++ src/main/kotlin/net/justlime/limeframegui/utilities/FrameColor.kt
@@ -23,7 +23,8 @@ object FrameColor {
      * Always returns a String — suitable for GUI APIs (1.8+ safe).
      */
     fun applyColor(text: String, player: Player? = null): String {
-        val newText = if (isPlaceholderAPIEnabled && player != null) PlaceholderAPI.setPlaceholders(player, text) else return text
+        val newText = if (isPlaceholderAPIEnabled && player != null) PlaceholderAPI.setPlaceholders(player, text.customPlaceholder(player.name))
+        else text.customPlaceholder(player?.name)
         return when (colorType) {
             ColorType.LEGACY -> ChatColor.translateAlternateColorCodes('&', newText)
             ColorType.HEX -> translateHexToLegacy(newText)
@@ -39,6 +40,11 @@ object FrameColor {
         return this.replace("§0", "<black>").replace("§1", "<dark_blue>").replace("§2", "<dark_green>").replace("§3", "<dark_aqua>").replace("§4", "<dark_red>").replace("§5", "<dark_purple>").replace("§6", "<gold>").replace("§7", "<gray>")
             .replace("§8", "<dark_gray>").replace("§9", "<blue>").replace("§a", "<green>").replace("§b", "<aqua>").replace("§c", "<red>").replace("§d", "<light_purple>").replace("§e", "<yellow>").replace("§f", "<white>").replace("§l", "<bold>")
             .replace("§m", "<strikethrough>").replace("§n", "<underlined>").replace("§o", "<italic>").replace("§r", "<reset>")
+    }
+
+    private fun String.customPlaceholder(name: String?): String {
+        if (name == null) return this
+        return this.replace("{player}", name)
     }
 
     fun applyColor(text: List<String>, player: Player? = null): List<String> {
